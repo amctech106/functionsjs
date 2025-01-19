8e0d8170f76ea69325057a59e52ae1ab19b70c75
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-01-19 10:57:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function myFunction (){
+    console.log("Abdul Majeed");
+    console.log("AMC Tech");
+    console.log("Abdul Majeed");
+    console.log("AMC Tech");
+}
+
+myFunction();
+myFunction();
+
+
+
+//// Aerow Function 
+
+
+const arrowMul = ( a , b) => {
+    console.log(a * b);
+}
